2aa1c5abaf8a1ff31fa5467ea80f20b488d320f0
pavansaikiran-18 <nerellapavansaikiran@gmail.com>
2025-12-27 23:14:41 +0530

    Modi: prepocess training modified with new data

---
+++ src/preprocess.py
@@ -0,0 +1,13 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+STOPWORDS = set(stopwords.words('english'))
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z ]', '', text)
+    words = text.split()
+    words = [w for w in words if w not in STOPWORDS]
+    return " ".join(words)
